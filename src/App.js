036e6b6e53345db2fa69540d2acc697aa472a3f7
handrawanw <handrawanw@gmail.com>
2020-04-05 01:29:30 +0700

    update

--- src/App.js
+++ src/App.js
@@ -1,14 +1,10 @@
 import React from 'react';
 import Dashboard from "./page/Dashboard";
 
-import {BrowserRouter as Router,Route} from "react-router-dom";
-
 function App() {
 
   return (
-    <Router>
-      <Route path="/" component={Dashboard} exact />
-    </Router>
+    <Dashboard />
   );
 }
 
